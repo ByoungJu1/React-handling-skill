298927e950ba51ec7dd17b884b27da9843318d5b
Byoungju <byoungju15@gmail.com>
2023-04-09 22:43:41 +0900

    &&연산자

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {Fragment} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const name = "리액트"
+    return (
+      <>
+        {name === "리액트" && (
+          <h1>{name} 안녕!</h1>
+        )}        
+      </>
+    );
 }
-
+// &&연산자 사용 시 0은 falsy한 값이지만 예외적으로 화면에 출력된다!
+// JSX로 괄호를 감싸는 것은 필수사항이 아니며 일반적으로 여러줄 사용 시 괄호로 감싼다.
+// jsx 코드없이 undefined를 반환하면 에러가 발생한다! 
 export default App;
